b312f06133f45438d71fee6441fc7be92a25aeb0
Hanna <96841662+greatHannahV@users.noreply.github.com>
2023-07-19 21:42:34 +0300

    servings

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -12,6 +12,31 @@ export default class View {
         this._parentElement.insertAdjacentHTML('afterbegin', markup);
 
     }
+    update(data) {
+        if (!data || (Array.isArray(data) && data.length === 0)) return this.renderError();
+
+        this._data = data;
+        const newMarkup = this._generateMarkup();
+
+        const newDOM = document.createRange().createContextualFragment(newMarkup);
+        const newElements = Array.from(newDOM.querySelectorAll('*'));
+        const currentElements = Array.from(this._parentElement.querySelectorAll('*'));
+
+        newElements.forEach((newEl, i) => {
+            const curEl = currentElements[i];
+
+            //updates chaged text
+            if (!newEl.isEqualNode(curEl) && newEl.firstChild.nodeValue.trim() !== '') {
+                curEl.textContent = newEl.textContent;
+            }
+            //updates changed attributes
+            if (!newEl.isEqualNode(curEl)) {
+                Array.from(newEl.attributes)
+                    .forEach(attr => curEl.setAttribute(attr.name, attr.value))
+
+            }
+        })
+    }
     _clear() {
         this._parentElement.innerHTML = '';
     }
